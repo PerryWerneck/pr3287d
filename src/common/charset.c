e75cfb83e8d4c04d8969dea39a618ad79f1dda0d
Perry Werneck <perry.werneck@gmail.com>
2023-05-03 10:02:14 -0300

    Fixing win32 charset.

--- src/common/charset.c
+++ src/common/charset.c
@@ -65,51 +65,25 @@ char *converters = CN;
 enum cs_result
 charset_init(char *csname)
 {
-#if !defined(_WIN32) /*[*/
-	char *codeset_name;
-#endif /*]*/
+	
 	const char *host_codepage;
-    	const char *cgcsgid_str;
+    const char *cgcsgid_str;
 	const char *display_charsets;
 
-#if !defined(_WIN32) /*[*/
-	setlocale(LC_ALL, "");
-	codeset_name = nl_langinfo(CODESET);
-#if defined(__CYGWIN__) /*[*/
-	/*
-	 * Cygwin's locale support is quite limited.  If the locale
-	 * indicates "US-ASCII", which appears to be the only supported
-	 * encoding, ignore it and use the Windows ANSI code page, which
-	 * observation indicates is what is actually supported.
-	 *
-	 * Hopefully at some point Cygwin will start returning something
-	 * meaningful here and this logic will stop triggering.
-	 *
-	 * If this (lack of) functionality persists, then it will probably
-	 * become necessary for pr3287 to support the wpr3287 '-printercp'
-	 * option, so that the printer code page can be configured.
-	 */
-	if (!strcmp(codeset_name, "US-ASCII")) {
-		codeset_name = Malloc(64);
-		sprintf(codeset_name, "CP%d", GetACP());
+#if defined(_WIN32)
+	{
+		char codeset_name[64];
+		memset(codeset_name,0,sizeof(codeset_name)); 
+		snprintf(codeset_name, 63, "CP%d", GetACP());
+		set_codeset(codeset_name);
 	}
-#endif /*]*/
-	set_codeset(codeset_name);
-#endif /*]*/
-
-    	if (set_uni(csname, &host_codepage, &cgcsgid_str,
-		    &display_charsets) < 0)
-		return CS_NOTFOUND;
-	cgcsgid = strtoul(cgcsgid_str, NULL, 0);
-	if (!(cgcsgid & ~0xffff))
-	    	cgcsgid |= 0x02b90000;
-
-#if defined(X3270_DBCS) /*[*/
-	if (set_uni_dbcs(csname, &cgcsgid_str, &display_charsets) == 0) {
-	    	dbcs = 1;
-		cgcsgid_dbcs = strtoul(cgcsgid_str, NULL, 0);
+#else 
+	{
+		setlocale(LC_ALL, "");
+		char *codeset_name = nl_langinfo(CODESET);
+		set_codeset(codeset_name);
 	}
-#endif /*]*/
+#endif
 
 	return CS_OKAY;
 }
