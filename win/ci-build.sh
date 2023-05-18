266344b2d709e67417192cb4c2cf1a8c13b443d4
Perry Werneck <perry.werneck@gmail.com>
2023-05-18 10:29:47 -0300

    Debugging msys action.

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -7,7 +7,11 @@
 #
 echo "Running ${0}"
 
-cd $(dirname $(dirname $(readlink -f ${0})))
+. "$(dirname $(readlink -f ${0}))/bundle.common"
+
+if [ -z ${PACKAGE_NAME} ]; then
+	exit -1
+fi
 
 #
 # Build PDFGEN
@@ -18,10 +22,12 @@ git clone https://github.com/AndreRenaud/PDFGen ./pdfgen || die "clone PDFGEN fa
 
 sed -i -e "s@^EXE_SUFFIX=.*@EXE_SUFFIX=.exe@g" ./pdfgen/Makefile || die "PDFGEN patch failure"
 sed -i -e "s@Windows_NT@DISABLED_OS@g" ./pdfgen/Makefile || die "PDFGEN patch failure"
+sed -i -e "s@-fprofile-arcs@@g" ./pdfgen/Makefile || die "PDFGEN patch failure"
+sed -i -e "s@-ftest-coverage@@g" ./pdfgen/Makefile || die "PDFGEN patch failure"
 
 make -C pdfgen || die "Make failure"
 
-ar rcs ${MINGW_PREFIX}/libpdfgen.a pdfgen/*.o
+ar rcs ${MINGW_PREFIX}/lib/libpdfgen.a pdfgen/*.o
 
 #
 # Build PR3287
@@ -29,8 +35,26 @@ ar rcs ${MINGW_PREFIX}/libpdfgen.a pdfgen/*.o
 export PDFGEN_CFLAGS=""
 export PDFGEN_LIBS="-lpdfgen"
 
-make all
+./autogen.sh
+if [ "$?" != "0" ]; then
+	exit -1
+fi
 
+build_package
+install_bin
+install_license
+
+#
+# Bundle
+#
+echo zip -9 -r -j "${PACKAGE_NAME}-${PACKAGE_VERSION}.${MSYSTEM_CARCH}.zip" "${buildroot}"
+
+zip -9 -r -j "${PACKAGE_NAME}-${PACKAGE_VERSION}.${MSYSTEM_CARCH}.zip" "${buildroot}"
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+ls -l *.zip
 echo "Build complete"
 
 
